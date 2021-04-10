b4f82e5beb6f83890a5fccb7f15455768c1bc167
Jared Fitts <jfitts@gmail.com>
2021-04-10 11:39:43 -0600

    added forms and providers

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+29 lines (27 sloc)  956 Bytes
+  
+import { Switch, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import Home from './components/shared/Home';
+import Navbar from './components/shared/Navbar';
+import NoMatch from './components/shared/NoMatch';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import FetchUser from './components/auth/FetchUser';
+import Wishes from './components/wishlist/Wishlists';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Navbar />
+    <FetchUser>
+      <Container>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <ProtectedRoute exact path="/wishes" component={Wishes} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Container>
+    </FetchUser>
+  </>
+)
 
 export default App;
---
+++ client/src/components/items/ItemForm.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { Form } from 'semantic-ui-react';
+import { ItemConsumer } from '../../providers/ItemProvider';
+
+const ItemForm = ({ additem }) => {
+  const [item, setItem] = useState({ name: "", description: "", user_id: ""})
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addItem(itemlist)
+    setItem({ name: "", description: "", user_id: ""})
+  }
+  return(
+    <Form onSubmit={handleSubmit}>
+      <Form.Input
+        label='Name'
+        name='name'
+        value={item.name}
+        onChange={(e, {value}) => setItem({...item, name: value})}
+        required
+        />
+        <Form.Input
+        label='Price'
+        name='price'
+        value={item.price}
+        onChange={(e, {value}) => setItem({...item, price: value})}
+        required
+        />
+    <Form.Button>Save</Form.Button>
+    </Form>
+  )
+}
+
+const ConnectedItemForm = (props) => (
+  <ItemConsumer>
+    { value => (
+      <ItemForm {...props} {...value} />
+    )}
+  </ItemConsumer>
+)
+
+export default ConnectedItemForm;
