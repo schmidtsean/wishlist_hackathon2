bca959239c770b4a75f1e6808d5408dbb3fd748d
Jared Fitts <jfitts@gmail.com>
2021-04-10 10:40:07 -0600

    added views

---
+++ client/src/components/catalog/Catalogs.js
@@ -0,0 +1,10 @@
+import WishesForm from './WishesForm'
+
+const Catalogs = () => (
+<>
+<h1>Catalogs</h1>
+<CatalogForm/>
+</>
+
+)
+export default Catalogs
---
+++ client/src/components/wishlist/Wishes.js
@@ -0,0 +1,10 @@
+import WishesForm from './WishesForm'
+
+const Wishes = () => (
+<>
+<h1>Wishes</h1>
+<WishesForm/>
+</>
+
+)
+export default Wishes
